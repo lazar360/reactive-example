61abe2c8f1c7de83d1a0d492c4f968ca7881aa18
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-04-23 15:24:21 +0200

    JavaCertification update

---
+++ src/Exercise1.java
@@ -0,0 +1,42 @@
+import java.util.stream.Stream;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        // Use StreamSources
+
+        // Print all numbers in the intNumberStream stream
+        System.out.println("Print all numbers in the intNumberStream stream");
+        StreamSources.intNumbersStream().forEach(System.out::println);
+
+        // Print numbers from intNumberStream that are less than 5
+        System.out.println("Print numbers from intNumberStream that are less than 5");
+        StreamSources.intNumbersStream().filter(number -> number<5).forEach(System.out::println);
+
+        // Print the second and third numbers in intNumberStream that's greater than 5 // 8 and 10
+        System.out.println("Print the second and third numbers in intNumberStream that's greater than 5");
+        StreamSources.intNumbersStream()
+                .filter(number -> number>5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+
+        // Print the first number in intNumberStream that's greater than 5
+        System.out.println("Print the first number in intNumberStream that's greater than 5");
+        StreamSources.intNumbersStream()
+                .filter(number -> number > 5)  // Check for numbers greater than 5
+                .findFirst()
+                .ifPresentOrElse(System.out::println, () -> System.out.println(-1));
+
+        // Print first names of all users in userStream
+        System.out.println("Print first names of all users in userStream");
+        StreamSources.userStream().forEach(user -> System.out.println(user.getFirstName()));
+
+
+
+
+
+
+    }
+}
